129b12d9dd26200da095ad3da8ab022a15c4655a
dylan-dolmazon <dylan.dolmazon@icloud.com>
2022-05-10 15:54:56 +0200

    authentification possible

--- project/src/Controller/LoginController.php
+++ project/src/Controller/LoginController.php
@@ -5,23 +5,28 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LoginController extends AbstractController
 {
-
     /**
      * @Route("/login/root")
      */
-    public function login(?User $user, Request $request, NotifierInterface $notifier, Session $session): Response
+    public function login(?User $user, Request $request, NotifierInterface $notifier, SessionInterface $session): Response
     {
-        if (!$session->start()) {
+
+
+        if ($session->get('connected') === null) {
+            $session->set('connected', false);
+        }
+        if ($session->get('connected') === false) {
             if (!$user) {
                 $user = new User();
             }
@@ -34,31 +39,28 @@ class LoginController extends AbstractController
                 if ($user === null) {
                     $notifier->send(new Notification("L'utilisateur que vous recherchez n'existe pas", ['browser']));
                 } else {
-                    $session->start();
-                    $session->set('user', $user->getUsername());
-                    $session->set('role', $user->getRole());
+                    $session->set('connected', true);
+                    $session->save();
                     return $this->render('login/connected.html.twig', [
-                        'username' => $session->get('user'),
-                        'role' => $session->get('role'),
+                        'username' =>  $user->getUsername(),
+                        'role' => $user->getRole(),
                     ]);
                 }
             }
-
             return $this->render('login/index.html.twig', [
                 'form' => $form->createView(),
             ]);
         } else {
             return $this->render('login/connected.html.twig', [
-                'username' => $session->get('user'),
-                'role' => $session->get('role'),
+                'username' =>  "coucou",
+                'role' => "coucou",
             ]);
         }
     }
 
-    public function logout(Session $session, ?User $user, Request $request)
+    public function logout(SessionInterface $session)
     {
-        $session->clear();
-        $router = new UrlGeneratorInterface();
-        return $router->generate('evenement');
+        $session->set('connected', false);
+        return new RedirectResponse($this->generateUrl('login'));
     }
 }
