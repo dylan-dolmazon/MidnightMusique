41acba25ee4c121c83ada7cde22399f64b3b7753
dylan-dolmazon <dylan.dolmazon@icloud.com>
2022-05-17 10:11:27 +0200

    css ajout avis

--- project/src/Controller/AvisController.php
+++ project/src/Controller/AvisController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Avis;
-use DateTime;
-use Doctrine\Migrations\Version\Direction;
+use App\Form\AvisType;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AvisController extends AbstractController
 {
-    public function index(PaginatorInterface $paginator, Request $request): Response
+    public function index(PaginatorInterface $paginator, Request $request, ?Avis $datas, EntityManagerInterface $entityManagerInterface): Response
     {
 
         $donnees = $this->getDoctrine()->getRepository(Avis::class)->findBy(array(), array('createdAt' => 'ASC'));
@@ -20,11 +21,34 @@ class AvisController extends AbstractController
         $avis = $paginator->paginate(
             $donnees,
             $request->query->getInt('page', 1),
-            4
+            5
         );
 
+        if(!$datas){
+            $datas = new Avis();
+        }
+
+        $form = $this->createForm(AvisType::class, $datas);
+
+        $form->add('save', SubmitType::class, array(
+            'attr' => array(
+                'class' => 'glow-on-hover'
+            )
+        ));
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$datas->getId()) {
+                dump($datas);
+                die;
+                $entityManagerInterface->persist($datas);
+            }
+            $entityManagerInterface->flush();
+        }
+
         return $this->render('avis/index.html.twig', [
-            'avis' => $avis
+            'avis' => $avis,
+            'form' => $form->createView(),
         ]);
     }
 }
--- project/src/Form/AvisType.php
+++ project/src/Form/AvisType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Avis;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,26 @@ class AvisType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('occasion')
-            ->add('content')
-            ->add('createdAt')
-            ->add('auteur')
+            ->add('occasion',TextType::class,array(
+                'attr' => array(
+                    'placeholder' => 'Occasion'
+                )
+            ))
+            ->add('content',TextareaType::class,array (
+                'attr' => array(
+                'placeholder' => 'Occasion'
+                )
+            ))
+            ->add('auteur',TextType::class,array (
+                'attr' => array(
+                    'placeholder' => 'Nom'
+                    )
+                ))
+            ->add('note',NumberType::class,array (
+                'attr' => array(
+                    'placeholder' => 'Note/5'
+                ))
+                )
         ;
     }
 
