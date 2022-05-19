693b6e644ccc1a6d97fc7b3f924b884dd7d68bd6
dylan-dolmazon <dylan.dolmazon@icloud.com>
2022-05-19 16:25:39 +0200

    debut feuille acceuil

--- project/src/Controller/AcceuilController.php
+++ project/src/Controller/AcceuilController.php
@@ -4,13 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AcceuilController extends AbstractController
 {
-    /**
-     * @Route("/acceuil", name="app_acceuil")
-     */
     public function index(): Response
     {
         return $this->render('acceuil/index.html.twig', [
