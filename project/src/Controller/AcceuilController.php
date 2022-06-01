abbe8add00796f3b3542d94126e0627f0e4d3c57
dylan-dolmazon <dylan.dolmazon@icloud.com>
2022-06-01 16:37:07 +0200

    ajout design contact

--- project/src/Controller/AcceuilController.php
+++ project/src/Controller/AcceuilController.php
@@ -4,11 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class AcceuilController extends AbstractController
 {
-    public function index(): Response
+    public function index(MailerInterface $mailer): Response
     {
+
+        /*$email = (new Email())
+            ->from('from@demo.fr')
+            ->to('to@demo.fr')
+            ->subject('Le petit sujet')
+            ->text('ceci est le text du mail')
+        ;
+
+        $mailer->send($email);*/
+
         return $this->render('acceuil/index.html.twig', [
             'controller_name' => 'AcceuilController',
         ]);
