2ca87f0fe7244d0c6d0f8c3f13742b8ad3847358
dylan-dolmazon <dylan.dolmazon@icloud.com>
2022-06-01 16:58:50 +0200

    form contact actif

--- project/src/Controller/AcceuilController.php
+++ project/src/Controller/AcceuilController.php
@@ -3,24 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
 class AcceuilController extends AbstractController
 {
-    public function index(MailerInterface $mailer): Response
+    public function index(): Response
     {
 
-        /*$email = (new Email())
-            ->from('from@demo.fr')
+        return $this->render('acceuil/index.html.twig', [
+            'controller_name' => 'AcceuilController',
+        ]);
+    }
+
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+
+        $email = (new Email())
+            ->from($request->request->get('email'))
             ->to('to@demo.fr')
-            ->subject('Le petit sujet')
-            ->text('ceci est le text du mail')
+            ->subject($request->request->get('sender'))
+            ->text($request->request->get('message'))
         ;
 
-        $mailer->send($email);*/
-
+        $mailer->send($email);
         return $this->render('acceuil/index.html.twig', [
             'controller_name' => 'AcceuilController',
         ]);
